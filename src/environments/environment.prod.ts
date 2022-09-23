8b20cb8031364ab56f925c8d27326d684f6e7783
nolangh <nolanghiggins@gmail.com>
2022-09-23 11:11:36 -0600

    Setup FireBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  production: true,
-  //* For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  firebaseConfig: {
+  firebase: {
+    projectId: 'notflix-1de69',
+    appId: '1:876698212555:web:4b69b98a1feae60279634e',
+    storageBucket: 'notflix-1de69.appspot.com',
     apiKey: 'AIzaSyBDG4SMqXp7ADk29oB-K__O3UiheFAe-c4',
     authDomain: 'notflix-1de69.firebaseapp.com',
-    projectId: 'notflix-1de69',
-    storageBucket: 'notflix-1de69.appspot.com',
     messagingSenderId: '876698212555',
-    appId: '1:876698212555:web:4b69b98a1feae60279634e',
     measurementId: 'G-E8RFM9DXNQ',
   },
+  production: true,
+  //* For Firebase JS SDK v7.20.0 and later, measurementId is optional
 };
