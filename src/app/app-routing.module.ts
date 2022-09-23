528a29f38f4995e9faae531091d0a4116567a272
nolangh <nolanghiggins@gmail.com>
2022-09-23 12:46:28 -0600

    Hopefully this restores everything

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'notflix',
+    pathMatch: 'full',
+  },
+  {
+    path: 'movies',
+    loadChildren: () =>
+      import('./movie/movie.module').then((m) => m.MovieModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
