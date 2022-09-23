5be940d65d56428fb0116b51c26064ddadb09155
nolangh <nolanghiggins@gmail.com>
2022-09-23 11:12:23 -0600

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
+  production: true,
+
+  //* For Firebase JS SDK v7.20.0 and later, measurementId is optional
   firebase: {
     projectId: 'notflix-1de69',
     appId: '1:876698212555:web:4b69b98a1feae60279634e',
@@ -8,6 +11,4 @@ export const environment = {
     messagingSenderId: '876698212555',
     measurementId: 'G-E8RFM9DXNQ',
   },
-  production: true,
-  //* For Firebase JS SDK v7.20.0 and later, measurementId is optional
 };
