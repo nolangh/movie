5d9d882fb47ffb3c7357cdad3441b67b4a54334a
nolangh <nolanghiggins@gmail.com>
2022-09-23 12:58:15 -0600

    movie-list works

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieListComponent } from './movie-list/movie-list.component';
-
-
+import { MovieRoutingModule } from './movie-list/movie-routing.module';
 
 @NgModule({
-  declarations: [
-    MovieListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MovieListComponent],
+  imports: [CommonModule, MovieRoutingModule],
 })
-export class MovieModule { }
+export class MovieModule {}
