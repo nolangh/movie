0ee8939d550b54f2d908da9453f00ff00add2439
nolangh <nolanghiggins@gmail.com>
2022-09-23 12:43:17 -0600

    fixed file structure for routing

---
+++ src/app/movie/movie-list/movie-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './movie-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MovieListComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class MovieRoutingModule {}
