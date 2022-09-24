f7b1da4043ebd2f1ee2df0457e5800d439bb1c18
nolangh <nolanghiggins@gmail.com>
2022-09-24 17:53:12 -0600

    -a

---
+++ src/app/movie/models/movie.model.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+  id: number;
+  name: string;
+  genre: string;
+  image: string;
+  releaseYear: number; //* If release year displays incoorectly change this to string
+}
