ccc4f6eded4835cd1cb3cbf3a2c096402dd7073c
nolangh <nolanghiggins@gmail.com>
2022-09-25 16:09:32 -0600

    Succsefully pulled movie data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'movies',
+    redirectTo: 'movie-list', // maybe change back to movies
     pathMatch: 'full',
   },
   {
-    path: 'movies',
+    path: 'movie-list',
     loadChildren: () =>
       import('./movie/movie.module').then((m) => m.MovieModule),
   },
--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { Movies } from '../models/movie.model';
 import { environment } from 'src/environments/environment';
 
+//* ANCHOR look into making these methods observables
+
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
@@ -16,13 +18,19 @@ export class MovieListComponent implements OnInit {
 
   constructor(private movieService: MovieService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getLatestMovies();
+    this.getPopularMovies();
+    //* TODO add all methods here so data is available on page load
+  }
 
   //* Fetch popular Movies
+  //* TODO Copy this methode for fetching other data
   getPopularMovies() {
     this.movieService.getPopularMovies().subscribe(
       (res) => {
         this.popularMovies = this.modifyData(res);
+        console.table(this.popularMovies);
       },
       (err) => {
         console.log('Not Able to fetch Movies', err);
@@ -35,6 +43,7 @@ export class MovieListComponent implements OnInit {
     this.movieService.getLatestMovie().subscribe(
       (res) => {
         this.latestMovies = res;
+        console.table(this.latestMovies);
       },
       (err) => {
         console.table("Couldn't fetch Latest Movies", err);
@@ -44,12 +53,15 @@ export class MovieListComponent implements OnInit {
 
   modifyData(movies: Movies): Movies {
     if (movies.results) {
-      movies.forEach((element: any) => {
+      movies.results.forEach((element: any) => {
         element.backdrop_path =
           'https://image.tmdb.org/t/p/original' +
           element.backdrop_path +
           'api_key?' +
           environment.movieApikey;
+        if (!element.title) {
+          element.title = element.name;
+        }
       });
     }
     return movies;
